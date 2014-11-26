cab1e70553c0c130606398edd6b31056f88916c2
Peter Orum <peter@peterorum.com>
2014-11-26 23:29:36 +1100

    Add /config url

--- server.js
+++ server.js
@@ -1,85 +1,93 @@
 (
     // serve up dynamic content
 
-    function ()
+    function()
     {
         // this code returns a web page with a word, ignoring the path
 
         "use strict";
 
-        var http = require( 'http' );
-        var fs = require( 'fs' );
+        var http = require('http');
+        var url = require('url');
+        var fs = require('fs');
 
         // var _ = require( 'lodash' );
         // _.templateSettings.interpolate = /{{([\s\S]+?)}}/g; // angular format
 
-        var rword = require( './get-word.js' );
-        var config = require( './process.js' );
+        var rword = require('./get-word.js');
+        var config = require('./process.js');
 
-        http.createServer( function ( request, response )
+        http.createServer(function(request, response)
         {
-            var word = rword.getWord();
+            var urlObj = url.parse(request.url, true, false);
 
-            response.setHeader( "Content-Type", "text/html" );
-            response.writeHead( 200 );
-            response.write( '<html>\n' );
-            response.write( '<head>\n' );
-            response.write( '<title>Random Inspiration</title>\n' );
-            response.write( '<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />\n' );
-            response.write( '<body>\n' );
+            response.setHeader("Content-Type", "text/html");
+            response.writeHead(200);
+            response.write('<html>\n');
+            response.write('<head>\n');
+            response.write('<title>Random Inspiration</title>\n');
+            response.write('<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />\n');
+            response.write('<body>\n');
 
-            // start container
-            response.write( '<div class="container">\n' );
+            if (urlObj.pathname === '/')
+            {
+                var word = rword.getWord();
 
-            // heading
-            response.write( '<div class="row">\n' );
-            response.write( '<div class="col-xs-12">\n' );
+                // start container
+                response.write('<div class="container">\n');
 
-            response.write( '<h1 class="text-center">Random Inspiration</h1>' );
+                // heading
+                response.write('<div class="row">\n');
+                response.write('<div class="col-xs-12">\n');
 
-            response.write( '</div>\n' );
-            response.write( '</div>\n' );
+                response.write('<h1 class="text-center">Random Inspiration</h1>');
 
-            // content
-            response.write( '<div class="row">\n' );
-            response.write( '<div class="col-xs-12">\n' );
+                response.write('</div>\n');
+                response.write('</div>\n');
 
-            response.write( '<h2 class="text-center">' );
-            // response.write( _.template( '<a href="http://www.thefreedictionary.com/{{ word }}" target="_blank">{{ word }}</a>',
-            // {
-            //     word: word
-            // } ) );
+                // content
+                response.write('<div class="row">\n');
+                response.write('<div class="col-xs-12">\n');
 
-            response.write( word );
-            response.write( '</h2>\n' );
+                response.write('<h2 class="text-center">');
+                // response.write( _.template( '<a href="http://www.thefreedictionary.com/{{ word }}" target="_blank">{{ word }}</a>',
+                // {
+                //     word: word
+                // } ) );
 
-            response.write( '</div>\n' );
-            response.write( '</div>\n' );
+                response.write(word);
+                response.write('</h2>\n');
 
-            // button
-            response.write( '<div class="row">\n' );
-            response.write( '<div class="col-xs-12 text-center">\n' );
+                response.write('</div>\n');
+                response.write('</div>\n');
 
-            response.write( '<div class="btn btn-primary" onclick="location.reload();">' );
-            response.write( 'again' );
-            response.write( '</div>\n' );
+                // button
+                response.write('<div class="row">\n');
+                response.write('<div class="col-xs-12 text-center">\n');
 
-            response.write( '</div>\n' );
-            response.write( '</div>\n' );
+                response.write('<div class="btn btn-primary" onclick="location.reload();">');
+                response.write('again');
+                response.write('</div>\n');
 
-            // end container
-            response.write( '</div>\n' );
+                response.write('</div>\n');
+                response.write('</div>\n');
 
-            //--------------------- config
+                // end container
+                response.write('</div>\n');
+            }
+            else if (urlObj.pathname === '/config')
+            {
+                //--------------------- config
 
-            response.write( '<pre>\n' );
-            response.write(JSON.stringify(config.getConfig(), null, 4));
-            response.write( '</pre>\n' );
+                response.write('<pre>\n');
+                response.write(JSON.stringify(config.getConfig(), null, 4));
+                response.write('</pre>\n');
+            }
 
-            response.write( '</body>\n' );
+            response.write('</body>\n');
 
             // end
-            response.end( '</html>\n' );
+            response.end('</html>\n');
 
-        } ).listen( process.env.PORT || 8888 );
-    }() );
+        }).listen(process.env.PORT || 8888);
+    }());
