7a58fd5ebcf40e25143486636e5040054c215e15
Peter Orum <peter@peterorum.com>
2014-12-12 00:31:36 +1100

    Add angular-hint

--- server.js
+++ server.js
@@ -41,11 +41,17 @@
 
                 response.setHeader("Content-Type", "text/html");
                 response.writeHead(200);
+
                 response.write('<html>\n');
+
+                // head
                 response.write('<head>\n');
                 response.write('<title>Random Inspiration</title>\n');
                 response.write('<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />\n');
-                response.write('<body ng-app="randomApp">\n');
+                response.write('</head>\n');
+
+                // body
+                response.write('<body ng-app="randomApp" ng-hint>\n');
 
                 if (uri.pathname === '/config')
                 {
@@ -102,7 +108,8 @@
                     response.write('</div>\n');
                 }
 
-                response.write('<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.2/angular.min.js"></script>\n');
+                response.write('<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.6/angular.min.js"></script>\n');
+                response.write('<script src="node_modules/angular-hint/dist/hint.js"></script>\n');
                 response.write('<script src="app/app.js"></script>\n');
 
                 response.write('</body>\n');
