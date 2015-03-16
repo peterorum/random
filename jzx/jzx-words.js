d569ef1d3a6356f774586bdcbad328ed59d8f39c
Peter Orum <peter@peterorum.com>
2015-03-16 21:33:23 +1100

    add words withno consonants

--- jzx/jzx-words.js
+++ jzx/jzx-words.js
@@ -2,21 +2,27 @@
 {
     "use strict";
 
-    var fp = require('lodash-fp');
+    var R = require('ramda');
+    var math = require('mathjs');
     var w = require('../get-word');
-
-    fp.mixin(require('../plus-fp/plus-fp'));
 
     // load the words array
     var words = w.getWords();
 
     // make function to test if a word os a particular length
-    var isLengthN = fp.curry(function(n, w)
+    var isLengthN = R.curry(function(n, w)
     {
         return w.length === n;
     });
 
     var isLength3 = isLengthN(3);
+
+    var isAtLeastLengthN = R.curry(function(n, w)
+    {
+        return w.length >= n;
+    });
+
+    var isAtLeastLength3 = isAtLeastLengthN(3);
 
     // test for j,k, q, x & z
     var hasValuableChar = function(w)
@@ -30,18 +36,22 @@
         return !/[aeiouy]/i.test(w);
     };
 
+    // also test for no consonants
+    var hasNoConsonant = function(w)
+    {
+        return !/[bcdfghjklmnpqrstvwxyz]/i.test(w);
+    };
+
     // want 3-letter words with a valuable character, or no vowel
-    var pick = fp.allOf([isLength3, fp.anyOf([hasValuableChar, hasNoVowely])]);
+    var pick1 = R.allPass([isLength3, R.anyPass([hasValuableChar, hasNoVowely])]);
+    var pick2 = R.allPass([isAtLeastLength3, hasNoConsonant]);
+    var pick = R.anyPass([pick1, pick2]);
 
     // get short valuable words
-    var w3valuable = fp.filter(pick, words);
+    var w3valuable = R.filter(pick, words);
 
     // dump all
+    // redirect to a text file or subl & copy into jzx-dict.js
     console.log(w3valuable);
-    console.log(w3valuable.length);
-
-    // pick one at random
-    var word = w3valuable[fp.random(0, w3valuable.length)];
-
-    console.log(word);
+    // console.log(w3valuable.length);
 }());
