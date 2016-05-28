6772c726d32c6ab7d69736e773fbc59606fad213
Peter Orum <peter@peterorum.com>
2016-05-28 13:40:11 +1000

    new words

--- jzx/jzx-words.js
+++ jzx/jzx-words.js
@@ -22,7 +22,15 @@
         return w.length >= n;
     });
 
+    var isAtMostLengthN = R.curry(function(n, w)
+    {
+        return w.length <= n;
+    });
+
     var isAtLeastLength3 = isAtLeastLengthN(3);
+
+    var isAtMostLength4 = isAtMostLengthN(4);
+    var isAtMostLength5 = isAtMostLengthN(5);
 
     // test for j,k, q, x & z
     var hasValuableChar = function(w)
@@ -42,10 +50,33 @@
         return !/[bcdfghjklmnpqrstvwxyz]/i.test(w);
     };
 
+    // q with u. skip plurals
+    var hasQNoU = function(w)
+    {
+        return /q/i.test(w) && !/u/i.test(w) && ! /s$/.test(w);
+    };
+
+    // 2 us
+    var has2Us = function(w)
+    {
+        var us = w.match(/u/g);
+
+        return us && us.length >= 2  && ! /s$/.test(w);
+    };
+
+    // begins with y consonant
+    var hasYconsonant = function(w)
+    {
+        return /^y[^aeiou]/.test(w)  && ! /s$/.test(w);
+    };
+
     // want 3-letter words with a valuable character, or no vowel
     var pick1 = R.allPass([isLength3, R.anyPass([hasValuableChar, hasNoVowely])]);
     var pick2 = R.allPass([isAtLeastLength3, hasNoConsonant]);
-    var pick = R.anyPass([pick1, pick2]);
+    var pick3 = R.allPass([hasQNoU]);
+    var pick4 = R.allPass([isAtMostLength4, has2Us]);
+    var pick5 = R.allPass([isAtMostLength5, hasYconsonant]);
+    var pick = R.anyPass([/*pick1, pick2,*/ pick3, pick4, pick5]);
 
     // get short valuable words
     var w3valuable = R.filter(pick, words);
@@ -53,8 +84,6 @@
     // dump all
     // redirect to a text file or subl & copy into jzx-dict.js
     console.log(w3valuable);
-    // console.log(w3valuable.length);
 
-    // todo q words without u
 
 }());
